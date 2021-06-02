335087f843c5640809c3011dce32df9579e4755b
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 17:18:11 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,39 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Log In</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<script type="text/javascript">
+<meta charset="utf-8"/>
+<title>Login</title>
+<link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-	<div id="logo">
-		<br>
-		<div style ="font-size: 35px;">Title for website</div>
-		<div style ="font-size: 20px;">Subtitle</div>
-		<br>
-	</div>
-	<br><br>
-	<div class="header">
-		<h2>Log In</h2>
-	</div>
-	<form method="post" action="login.php">
-
-		<?php echo display_error(); ?>
-
-		<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username" >
-		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="pass" name="password">	
-		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="login_btn">Login</button>
-		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
-	</form>
+<?php
+require('db.php');
+session_start();
+// When form submitted, check and create user session.
+if (isset($_POST['username'])) {
+$username = stripslashes($_REQUEST['username']); // removes backslashes
+$username = mysqli_real_escape_string($con, $username);
+$password = stripslashes($_REQUEST['password']);
+$password = mysqli_real_escape_string($con, $password);
+// Check user is exist in the database
+$query = "SELECT * FROM `users` WHERE username='$username'
+AND password='" . md5($password) . "'";
+$result = mysqli_query($con, $query) or die(mysql_error());
+$rows = mysqli_num_rows($result);
+if ($rows == 1) {
+$_SESSION['username'] = $username;
+// Redirect to user dashboard page
+header("Location: dashboard.php");
+} else {
+echo "<div class='form'>
+<h3>Incorrect Username/password.</h3><br/>
+<p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+</div>";
+}
+} else {
+?>
+<form class="form" method="post" name="login">
+<h1 class="login-title">Login</h1>
+<input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+<input type="password" class="login-input" name="password" placeholder="Password"/>
+<input type="submit" value="Login" name="submit" class="login-button"/>
+<p class="link"><a href="registration.php">New Registration</a></p>
+</form>
+<?php
+}
+?>
 </body>
 </html>
