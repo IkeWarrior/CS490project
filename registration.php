25690374333811808275b960abf1997dbce6093b
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 17:14:24 -0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,28 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Registration</title>
+<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
 <?php
-    session_start();
-    include('config.php');
-    if (isset($_POST['register'])) {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $password_hash = password_hash($password, PASSWORD_BCRYPT);
-        $query = $connection->prepare("SELECT * FROM users WHERE email=:email");
-        $query->bindParam("email", $email, PDO::PARAM_STR);
-        $query->execute();
-        if ($query->rowCount() > 0) {
-            echo '<p class="error">The email address is already registered!</p>';
-        }
-        if ($query->rowCount() == 0) {
-            $query = $connection->prepare("INSERT INTO users(username,password,email) VALUES (:username,:password_hash,:email)");
-            $query->bindParam("username", $username, PDO::PARAM_STR);
-            $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
-            $query->bindParam("email", $email, PDO::PARAM_STR);
-            $result = $query->execute();
-            if ($result) {
-                echo '<p class="success">Your registration was successful!</p>';
-            } else {
-                echo '<p class="error">Something went wrong!</p>';
-            }
-        }
-    }
+require('db.php');
+// When form submitted, insert values into the database.
+if (isset($_REQUEST['username'])) {
+// removes backslashes
+$username = stripslashes($_REQUEST['username']);
+//escapes special characters in a string
+$username = mysqli_real_escape_string($con, $username);
+$email = stripslashes($_REQUEST['email']);
+$email = mysqli_real_escape_string($con, $email);
+$password = stripslashes($_REQUEST['password']);
+$password = mysqli_real_escape_string($con, $password);
+$create_datetime = date("Y-m-d H:i:s");
+$query = "INSERT into `users` (username, password, email, create_datetime)
+VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
+$result = mysqli_query($con, $query);
+if ($result) {
+echo "<div class='form'>
+<h3>You are registered successfully.</h3><br/>
+<p class='link'>Click here to <a href='login.php'>Login</a></p>
+</div>";
+} else {
+echo "<div class='form'>
+<h3>Required fields are missing.</h3><br/>
+<p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+</div>";
+}
+} else {
 ?>
+<form class="form" action="" method="post">
+<h1 class="login-title">Registration</h1>
+<input type="text" class="login-input" name="username" placeholder="Username" required />
+<input type="text" class="login-input" name="email" placeholder="Email Adress">
+<input type="password" class="login-input" name="password" placeholder="Password">
+<input type="submit" name="submit" value="Register" class="login-button">
+<p class="link"><a href="login.php">Click to Login</a></p>
+</form>
+<?php
+}
+?>
+</body>
+</html>
